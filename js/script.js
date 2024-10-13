1c0b3904b395544fef9595bd35abf3cbf82ec30b
arthurviny <arthur.barreto@dcomp.ufs.br>
2024-10-12 23:03:48 -0300

    Adição da Lista de Palavras Mais umas parada

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const listadepalavras = ["MAGIA", "CARRO", "CASAS", "LIVRO", "FELIZ", "TEMPO", "FORTE", "PAZES", "VIRUS", "PLANO", 
+"JOGOS", "FLORE", "TRONO", "BRAVO", "DENTE", "CALOR", "SORVE", "NEVES", "FRUTA", "CINCO", 
+"MOEDA", "RISOS", "SONHO", "PRAZO", "VENTO", "CACHO", "TRAZE", "FOCAR", "VIDRO", "METRO", 
+"LAZER", "PALMA", "AMIGO", "FAZER", "NORTE", "LARGO", "GARFO", "CORPO", "NOBRE", "DANCA", 
+"RITMO", "BOTAO", "LOUSA", "CHUVA", "TERRA", "NUVEM", "TREVO", "ACENO", "RESTA", "SALTO", 
+"LINHA", "PERDA", "CANTO", "MUNDO", "BOLSA", "GOSTO", "VELHO", "LIMAO", "FACIL", "FRIO",
+]
+
+const palavraaleatoria = (palavra) => palavra[Math.floor(Math.random * palavra.length)] // Função que escolhe uma palavra aleatória por meio do índice dela
+
+
+
+
+ 
