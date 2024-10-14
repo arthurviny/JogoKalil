259ee2b6c0797be146ae8088bbbc3a8848925d5a
arthurviny <arthur.barreto@dcomp.ufs.br>
2024-10-14 20:46:36 -0300

    funções necessárias (caso tenha mais alguma podem add ai na lista)

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 const listadepalavras = ["MAGIA", "CARRO", "CASAS", "LIVRO", "FELIZ", "TEMPO", "FORTE", "PAZES", "VIRUS", "PLANO", 
-"JOGOS", "FLORE", "TRONO", "BRAVO", "DENTE", "CALOR", "SORVE", "NEVES", "FRUTA", "CINCO", 
-"MOEDA", "RISOS", "SONHO", "PRAZO", "VENTO", "CACHO", "TRAZE", "FOCAR", "VIDRO", "METRO", 
+"JOGOS", "FALSO", "TRONO", "BRAVO", "DENTE", "CALOR", "SORVE", "NEVES", "FRUTA", "CINCO", 
+"MOEDA", "RISOS", "SONHO", "PRAZO", "VENTO", "CACHO", "TREZE", "FOCAR", "VIDRO", "METRO", 
 "LAZER", "PALMA", "AMIGO", "FAZER", "NORTE", "LARGO", "GARFO", "CORPO", "NOBRE", "DANCA", 
 "RITMO", "BOTAO", "LOUSA", "CHUVA", "TERRA", "NUVEM", "TREVO", "ACENO", "RESTA", "SALTO", 
-"LINHA", "PERDA", "CANTO", "MUNDO", "BOLSA", "GOSTO", "VELHO", "LIMAO", "FACIL", "FRIO",
+"LINHA", "PERDA", "CANTO", "MUNDO", "BOLSA", "GOSTO", "VELHO", "LIMAO", "FACIL", "FRIOS",
 ]
 
-const palavraaleatoria = (palavra) => palavra[Math.floor(Math.random * palavra.length)] // Função que escolhe uma palavra aleatória por meio do índice dela
+const palavraaleatoria = (palavra) => palavra[Math.floor(Math.random()* palavra.length)] // Função que escolhe uma palavra aleatória por meio do índice dela
 
+// Criar Uma Função Para que os quadradinhos apareçam na tela após a pessoa digitar a palavra
+
+/*Função para verificar as letras, dizendo se a letra em questão está certa (letra certa no lugar certo),
+quase certa (letra está contida na palavra, porém posicionada no lugar errado ou não está nem na palavra*/
+
+/* Dado o valor em correta, quase e errada (nomes que eu defini no css), criar uma função para aplicar 
+tais classes em cada uma das letras */
+
+// função verificarpalavra que vai ver a palavra digitada pelo jogador e comparar com a palavra secreta
+
+// função pra reiniciar o jogo (ficar jogado ai até cansar  )
 
 
 
