0d18ac3c42e323220483648bdcedd0dbe5e525c0
arthurviny <arthur.barreto@dcomp.ufs.br>
2024-10-14 23:13:28 -0300

    organizadinha de leve

--- js/script.js
+++ js/script.js
@@ -9,14 +9,7 @@ const listadepalavras = ["SINTO", "SUITE", "AVIDO", "FESTA", "BEBIA", "HONRA", "
 const palavraAleatoria = () => listadepalavras[Math.floor(Math.random()* listadepalavras.length)] // Função que escolhe uma palavra aleatória por meio do índice dela
 
 const palavraSelecionada = palavraAleatoria() // Função que guarda durante o jogo o valor escolhido pela função palavraAleatoria
-
-function verificarpalavra () { //Função que verifica se a palavra atende ao requisito de possuir exatamente 5 letras e verifica se é igual a palavra selecionada ou se é diferente dela
-    const input = document.getElementById('entrada-palavra').value
-
-    if(input.length !== 5) {alert("A palavra deve ter exatamente 5 letras!")}
-        else if(input.toUpperCase() === palavraSelecionada) {alert("Parabéns, você acertou!")}
-            else {alert("Errou, tente novamente!")}
-}
+   
 //Criar Uma Função Para que os quadradinhos apareçam na tela após a pessoa digitar a palavra
 
 /*Função para verificar as letras, dizendo se a letra em questão está certa (letra certa no lugar certo),
@@ -25,7 +18,14 @@ quase certa (letra está contida na palavra, porém posicionada no lugar errado 
 /* Dado o valor em correta, quase e errada (nomes que eu defini no css), criar uma função para aplicar 
 tais classes em cada uma das letras */
 
-// função verificarpalavra que vai ver a palavra digitada pelo jogador e comparar com a palavra secreta
+
+function verificarpalavra () { //Função que verifica se a palavra atende ao requisito de possuir exatamente 5 letras e verifica se é igual a palavra selecionada ou se é diferente dela
+    const input = document.getElementById('entrada-palavra').value
+
+    if(input.length !== 5) {alert("A palavra deve ter exatamente 5 letras!")}
+        else if(input.toUpperCase() === palavraSelecionada) {alert("Parabéns, você acertou!")}
+            else {alert("Errou, tente novamente!")}
+}
 
 // função pra reiniciar o jogo (Unlimited mode)
 
